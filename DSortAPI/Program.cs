fd4b84dea4fb1ed802b27abd8f313022adde2e14
franciscoassumpcao <38674113+franciscoassumpcao@users.noreply.github.com>
2023-01-13 15:07:40 +0100

    mergint projects

--- DSortAPI/Program.cs
+++ DSortAPI/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<DataContext>(options =>
 {
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -22,13 +23,23 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 	{
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	app.UseSwaggerUI();	
 	}
 
-app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+    {
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    }
 
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapRazorPages();
 
 app.Run();
